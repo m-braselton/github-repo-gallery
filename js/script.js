41a8aafc93c1ebfe4e2dd05ce2f8d600238bcc6c
Marty Braselton <76597962+m-braselton@users.noreply.github.com>
2021-12-04 08:13:58 -0800

    async end points, for each loop

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //targetting the div where your profile info will appear.
 const overView = document.querySelector(".overview");
 const username = "m-braselton";
+const repoList = document.querySelector(".repo-list");
 
 //create and name an async function to fetch info from github
 const githubProfile = async function() {
@@ -11,6 +12,7 @@ const githubProfile = async function() {
 githubProfile();
 
 //function to display the fetched user information
+//create a div to contain user information
 const userInfo = function(data) {
   var div = document.createElement("div");
   div.classList.add ("user-info");
@@ -26,4 +28,23 @@ const userInfo = function(data) {
     </div>
   `;
   overView.append(div);
+  fetchRepos();
+
+};
+//async function to get list of users repos
+const fetchRepos = async function() {
+  const showRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await showRepos.json();
+    //console.log(repos);
+    displayInfo(repoData);
+};
+//create function to display info from each repo.
+const displayInfo = function(repos) {
+  //give each (loop) repo a class of "repo" and an <h3> element with name of repo
+  for(const repo of repos) {
+     const repoItem = document.createElement("li");
+     repoItem.classList.add("repo");
+     repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+     repoList.append(repoItem);
+  }
 };
